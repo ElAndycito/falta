490ffe666c8c38d1c9048bbdc58d2cb7dba6a8d6
B13-AINF <B13@academico.unab.ad>
2023-09-27 09:54:14 -0500

    pt1

---
+++ app/src/main/java/com/andresdiaz/falta/calendar2.java
@@ -0,0 +1,14 @@
+package com.andresdiaz.falta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calendar2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar2);
+    }
+}
